353af12ea0628ca011e6601fd90f86d7356ba2c2
Brett Mravec <brett.mravec@gmail.com>
2010-01-19 17:21:56 -0500

    license headers and start work on add download window

--- gui/toolbar.py
+++ gui/toolbar.py
@@ -1,4 +1,26 @@
+#
+#       toolbar.py
+#
+#       Copyright 2010 Brett Mravec <brett.mravec@gmail.com>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
 import gtk
+
+from addwindow import AddWindow
 
 class Toolbar (gtk.Toolbar):
     def __init__ (self, app):
@@ -30,6 +52,11 @@ class Toolbar (gtk.Toolbar):
         self.insert (self.pause_button, -1)
 
     def add_clicked (self, item):
+        awin = AddWindow ()
+        rid = awin.run ()
+
+        awin.destroy ()
+        """
         textview = gtk.TextView ()
 
         dialog = gtk.Dialog ('Add URLs...',
@@ -52,7 +79,7 @@ class Toolbar (gtk.Toolbar):
             self.app.downman.parse_download (text)
 
         dialog.destroy ()
-
+        """
     def remove_clicked (self, item):
         ds = self.app.dv.get_selected ()
         for d in ds:
