3669e29185d255c037f3edce8d2e676938c5d325
Brett Mravec <brett.mravec@gmail.com>
2010-02-26 14:44:24 -0500

    Create simple url class that handles parsing a url and returning host, port, etc.

---
+++ src/url.cc
@@ -0,0 +1,94 @@
+/*
+ *      url.cc
+ *
+ *      Copyright 2010 Brett Mravec <brett.mravec@gmail.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+#include <iostream>
+
+#include "url.h"
+#include "dregex.h"
+#include "utils.h"
+
+static DRegex *regex = new DRegex ("(\\S*)://([^/ \t\r\n\f\v]*)(\\S*)");
+
+Url::Url (std::string &url) : port (0)
+{
+    parse_url (url);
+}
+
+Url::Url (const char *url) : port (0)
+{
+    parse_url (url);
+}
+
+Url::Url () : port (0)
+{
+
+}
+
+Url::~Url ()
+{
+
+}
+
+void
+Url::parse_url (const char *url)
+{
+    std::string turl (url);
+
+    parse_url (turl);
+}
+
+void
+Url::parse_url (std::string &url)
+{
+    std::vector<std::string> results;
+    size_t pos;
+
+    url = url,
+    proto = "";
+    host = "";
+    port = 0;
+    path = "";
+    name = "";
+    ext = "";
+
+    if (regex->find (url, results)) {
+        proto = results[1];
+
+        host = results[2];
+
+        pos = host.find (":");
+        if (pos != std::string::npos) {
+            port = Utils::parseInt (host.c_str () + pos + 1);
+            host = host.substr (0, pos);
+        }
+
+        path = results[3];
+        pos = path.rfind ("/");
+        if (pos != std::string::npos) {
+            name = path.substr (pos + 1);
+        }
+
+        pos = name.rfind (".");
+        if (name.size () > 0 && pos != std::string::npos) {
+            ext = name.substr (pos + 1);
+        }
+    }
+}
---
+++ src/url.h
@@ -0,0 +1,52 @@
+/*
+ *      url.h
+ *
+ *      Copyright 2010 Brett Mravec <brett.mravec@gmail.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+class Url;
+
+#ifndef __URL_H__
+#define __URL_H__
+
+#include <string>
+
+class Url {
+    public:
+        Url (std::string &url);
+        Url (const char *url);
+        Url ();
+        ~Url ();
+
+        void parse_url (std::string &url);
+        void parse_url (const char *url);
+
+        std::string &get_url () { return url; }
+        std::string &get_host () { return host; }
+        int get_port () { return port; }
+        std::string &get_proto () { return proto; }
+        std::string &get_path () { return path; }
+        std::string &get_name () { return name; }
+        std::string &get_ext () { return ext; }
+
+    private:
+        std::string url, host, proto, path, name, ext;
+        int port;
+};
+
+#endif /* __URL_H__ */
