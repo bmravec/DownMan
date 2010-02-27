05611775c5de84ba7ed162f1bb420b1e21bcacaf
Brett Mravec <brett.mravec@gmail.com>
2010-02-27 00:08:01 -0500

    Implement SAX parser using libxml2 to import config.xml

--- src/app-config.cc
+++ src/app-config.cc
@@ -19,14 +19,196 @@
  *      MA 02110-1301, USA.
  */
 
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
 #include "app-config.h"
 
-Config::Config ()
-{
+#define DEFAULT_DOWNLOAD_DIRECTORY "~/Downloads"
 
+typedef enum {
+    STATE_NULL = 0,
+    STATE_DOWNMAN,
+    STATE_CONFIG,
+    STATE_CONFIG_PROPERTY,
+    STATE_DOWNLOAD,
+    STATE_DOWNLOAD_PROPERTY,
+    STATE_ERROR,
+} SaxParserState;
+
+typedef struct {
+    Config *config;
+    SaxParserState state;
+    std::string key, val;
+    std::map<std::string, std::string> tdownload;
+} SaxParserData;
+
+const std::string Config::MAX_DOWNLOAD_SPEED ("MaxDownloadSpeed");
+const std::string Config::MAX_UPLOAD_SPEED ("MaxUploadSpeed");
+const std::string Config::MAX_NUM_DOWNLOADS ("MaxNumDownloads");
+const std::string Config::DOWNLOAD_DIRECTORY ("DownloadDirectory");
+
+Config::Config () : downloads (new std::vector<std::map<std::string, std::string> >)
+{
+    set_property (DOWNLOAD_DIRECTORY, DEFAULT_DOWNLOAD_DIRECTORY);
+    set_property (MAX_UPLOAD_SPEED, "-1");
+    set_property (MAX_DOWNLOAD_SPEED, "-1");
+    set_property (MAX_NUM_DOWNLOADS, "1");
 }
 
 Config::~Config ()
 {
 
+}
+
+sigc::signal<void, const std::string&, const std::string&>&
+Config::get_signal (const std::string &key)
+{
+    return properties[key].sig;
+}
+
+std::string
+Config::get_property (std::string &key)
+{
+    return properties[key].val;
+}
+
+void
+Config::set_property (const std::string &key, const std::string &val)
+{
+    properties[key].val = val;
+
+    properties[key].sig (key, val);
+}
+
+void
+Config::set_property (const std::string &key, const char *val)
+{
+    std::string sval (val);
+
+    set_property (key, sval);
+}
+
+bool
+Config::load_settings ()
+{
+    xmlSAXHandlerPtr shandle = new xmlSAXHandler;
+    memset (shandle, 0, sizeof (xmlSAXHandler));
+
+    shandle->startElement = Config::ac_start_element;
+    shandle->endElement = Config::ac_end_element;
+    shandle->characters = Config::ac_characters;
+
+    SaxParserData *data = new SaxParserData;
+    data->config = this;
+    data->state = STATE_NULL;
+
+    char *home = getenv ("HOME");
+    std::string configpath (home);
+    configpath += "/.config/downman/config.xml";
+
+    xmlSAXUserParseFile (shandle, data, configpath.c_str ());
+
+    delete data;
+    delete shandle;
+}
+
+std::vector<std::map<std::string, std::string> >*
+Config::load_downloads ()
+{
+    if (downloads) {
+        std::vector<std::map<std::string, std::string> > *d = downloads;
+        downloads = NULL;
+        return d;
+    } else {
+        return NULL;
+    }
+}
+
+void
+Config::save (std::vector<std::map<std::string, std::string> > &dlist)
+{
+    std::cout << "Config::save (&dlist): stub\n";
+}
+
+void
+Config::ac_start_element (void *user_data, const xmlChar *name, const xmlChar **attrs)
+{
+    SaxParserData *sdata = (SaxParserData*) user_data;
+
+    std::string sname ((const char*) name);
+
+    switch (sdata->state) {
+        case STATE_NULL:
+            if (sname == "downman") {
+                sdata->state = STATE_DOWNMAN;
+            }
+
+            break;
+        case STATE_DOWNMAN:
+            if (sname == "config") {
+                sdata->state = STATE_CONFIG;
+            } else if (sname == "Download") {
+                sdata->state = STATE_DOWNLOAD;
+                sdata->tdownload.clear ();
+            }
+
+            break;
+        case STATE_CONFIG:
+            sdata->key = sname;
+            sdata->state = STATE_CONFIG_PROPERTY;
+            break;
+        case STATE_DOWNLOAD:
+            sdata->key = sname;
+            sdata->state = STATE_DOWNLOAD_PROPERTY;
+            break;
+    };
+}
+
+void
+Config::ac_end_element (void *user_data, const xmlChar *name)
+{
+    SaxParserData *sdata = (SaxParserData*) user_data;
+
+    std::string sname ((const char*) name);
+
+    switch (sdata->state) {
+        case STATE_DOWNMAN:
+            sdata->state = STATE_NULL;
+            break;
+        case STATE_CONFIG:
+            sdata->state = STATE_DOWNMAN;
+            break;
+        case STATE_CONFIG_PROPERTY:
+            sdata->state = STATE_CONFIG;
+            if (sdata->key == sname) {
+                sdata->config->set_property (sdata->key, sdata->val);
+            } else {
+                sdata->state = STATE_ERROR;
+            }
+
+            break;
+        case STATE_DOWNLOAD:
+            sdata->state = STATE_DOWNMAN;
+            sdata->config->downloads->push_back (sdata->tdownload);
+            break;
+        case STATE_DOWNLOAD_PROPERTY:
+            sdata->state = STATE_DOWNLOAD;
+            sdata->tdownload[sdata->key] = sdata->val;
+            break;
+    };
+}
+
+void
+Config::ac_characters (void *user_data, const xmlChar *ch, int len)
+{
+    SaxParserData *sdata = (SaxParserData*) user_data;
+
+    std::string str ((const char*) ch, len);
+
+    if (sdata->state == STATE_DOWNLOAD_PROPERTY ||
+        sdata->state == STATE_CONFIG_PROPERTY) {
+        sdata->val = str;
+    }
 }
--- src/app-config.h
+++ src/app-config.h
@@ -24,6 +24,18 @@ class Config;
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include <sigc++/sigc++.h>
+#include <libxml/parser.h>
+
+typedef struct {
+    std::string val;
+    sigc::signal<void, const std::string&, const std::string&> sig;
+} PropertySet;
+
 class Config {
     public:
         static Config &Instance () {
@@ -31,9 +43,31 @@ class Config {
             return config;
         }
 
+        sigc::signal<void, const std::string&, const std::string&> &get_signal (const std::string &key);
+
+        std::string get_property (std::string &key);
+        void set_property (const std::string &key, const std::string &val);
+        void set_property (const std::string &key, const char *val);
+
+        bool load_settings ();
+        std::vector<std::map<std::string, std::string> > *load_downloads ();
+        void save (std::vector<std::map<std::string, std::string> > &dlist);
+
+        static const std::string MAX_DOWNLOAD_SPEED;
+        static const std::string MAX_UPLOAD_SPEED;
+        static const std::string MAX_NUM_DOWNLOADS;
+        static const std::string DOWNLOAD_DIRECTORY;
+
     private:
         Config ();
         ~Config ();
+
+        static void ac_start_element (void *user_data, const xmlChar *name, const xmlChar **attrs);
+        static void ac_end_element (void *user_data, const xmlChar *name);
+        static void ac_characters (void *user_data, const xmlChar *ch, int len);
+
+        std::map<std::string, PropertySet> properties;
+        std::vector<std::map<std::string, std::string> > *downloads;
 };
 
 #endif /* __CONFIG_H__ */
