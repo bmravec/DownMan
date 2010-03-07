3642c829f31d5171f8d53046c620c5d6df0a75bf
Brett Mravec <brett.mravec@gmail.com>
2010-03-06 21:16:41 -0500

    Add doc comments to DownloadFactory

--- src/download-factory.h
+++ src/download-factory.h
@@ -27,12 +27,37 @@ class DownloadFactory;
 #include "download.h"
 #include "url.h"
 
+/**
+ * Factory to create generic Download objects.
+ * Factory class that creates Download objects and returns generic Download
+ * objects so the rest of the application can work with the common interface
+ * and not have to worry about the specifics of a Download instance.
+ */
 class DownloadFactory {
     public:
         DownloadFactory ();
         ~DownloadFactory ();
 
+        /**
+         * Create new a download from a url.
+         * Takes the url object and uses it to create a new download of a
+         * specific type.  It then returns a generic download object to be used
+         * by the rest of the program.
+         * @param url url to be used to create the specific download
+         * @return Generic Download object representing the given url or NULL if
+         *         the url does not describe a downloadable type of DownMan
+         */
         Download *create_download (Url &url);
+
+        /**
+         * Build a download from saved data.
+         * Takes a dictionary of data describing a saved download and creates
+         * the object in the saved state.
+         * @param data dictionary using Download::KEY_* as keys describing the
+         *             internal state of download to be loaded.
+         * @return Generic Download object loading using the input data or NULL
+                   if the data did not describe a valid Download object.
+         */
         Download *build_download (std::map<std::string, std::string> &data);
 
     private:
