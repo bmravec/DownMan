1db2533d05f46558b9f9860b98a7e78def892da3
Brett Mravec <brett.mravec@gmail.com>
2010-03-11 22:33:51 -0500

    Create some documentation for HttpConnection, change send_post_request to take a dictionary instead of a string, and clean up several other functions to conform to the standard described in the doc comments

--- src/http-connection.cc
+++ src/http-connection.cc
@@ -19,36 +19,58 @@
  *      MA 02110-1301, USA.
  */
 
-#include <iostream>
-#include <fstream>
+#include "../config.h"
+
 #include <cstring>
 
+#include "app-log.h"
 #include "http-connection.h"
 #include "utils.h"
 
-#define BUFFER_TOTAL 51200
+#define BUFFER_TOTAL 10240
+
+const std::string HttpConnection::USER_AGENT (PACKAGE_NAME "/" PACKAGE_VERSION);
 
 HttpConnection::HttpConnection () :
     url (NULL), socket (NULL), buff (new char [BUFFER_TOTAL]), bufflen (0), buffpos (0),
-    cpos (0), clength (-1)
+    cpos (0), clength (-1), response_code (-1)
 {
 
 }
 
 HttpConnection::~HttpConnection ()
 {
+    reset_connection ();
+
     delete buff;
+}
+
+void
+HttpConnection::reset_connection ()
+{
+    if (socket != NULL) {
+        delete socket;
+        socket = NULL;
+
+        bufflen = 0;
+        buffpos = 0;
+
+        response_code = -1;
+        code = "";
+    }
 }
 
 bool
 HttpConnection::send_get_request (Url &url, int start, int end)
 {
-    if (socket == NULL) {
-        socket = new Socket (url.get_host ().c_str (), url.get_port () != 0 ? url.get_port () : 80);
+    reset_connection ();
 
+    socket = new Socket (url.get_host ().c_str (), url.get_port () != 0 ? url.get_port () : 80);
+
+    if (socket->is_connected ()) {
         std::string request = "GET " + url.get_path () + " HTTP/1.1\r\n";
         request += "Host: " + url.get_host () + "\r\n";
-        request += "User-Agent: DownMan-BMT/1.0\r\n";
+        request += "User-Agent: " + USER_AGENT +"\r\n";
 
         if (start != -1) {
             request += "Range: bytes=";
@@ -60,39 +82,44 @@ HttpConnection::send_get_request (Url &url, int start, int end)
             request += "\r\n";
         }
 
-        request += "\r\n";
+        LOG_DEBUG ("Http Get Request:\n" + request);
 
-        std::cout << "Request: " << request;
+        request += "\r\n";
 
         socket->write (request);
 
         return read_header ();
-    } else {
-        return false;
     }
+
+    return false;
 }
 
 bool
 HttpConnection::send_head_request (Url &url)
 {
-    if (socket == NULL) {
-        socket = new Socket (url.get_host ().c_str (), url.get_port () != 0 ? url.get_port () : 80);
+    reset_connection ();
 
+    socket = new Socket (url.get_host ().c_str (), url.get_port () != 0 ? url.get_port () : 80);
+
+    if (socket->is_connected ()) {
         std::string request = "HEAD " + url.get_path () + " HTTP/1.1\r\n";
         request += "Host: " + url.get_host () + "\r\n";
-        request += "User-Agent: DownMan-BMT/1.0\r\n";
+        request += "User-Agent: " + USER_AGENT +"\r\n";
+
+        LOG_DEBUG ("Http Head Request:\n" + request);
+
         request += "\r\n";
 
         socket->write (request);
 
         return read_header ();
-    } else {
-        return false;
     }
+
+    return false;
 }
 
 bool
-HttpConnection::send_post_request (Url &url, const char *post_data)
+HttpConnection::send_post_request (Url &url, std::map<std::string, std::string> &post_data)
 {
     return false;
 }
@@ -107,15 +134,17 @@ HttpConnection::read_header ()
 
     header.clear ();
 
+    std::string ldata;
+
     version = std::string (buff, 8);
     response_code = Utils::parseInt (buff + 9);
     for (lend = 0; !(buff[lend] == '\n' || (lend != 0 && buff[lend - 1] == '\n' && buff[lend] == '\r')); lend++);
     code = std::string (buff + 13, lend - 14);
 
-    std::cout << "Version: " << version << std::endl;
-    std::cout << "Code: " << response_code << std::endl;
-    std::cout << "Code: " << code << std::endl;
+    LOG_DEBUG ("Http Response Version: " + version);
+    LOG_DEBUG ("Http Response Code: " + Utils::formatInt (response_code) + ", " + code);
 
+    std::string hstr ("Http Response Headers:\n");
     for (lbegin = lend = 0; true; lend++) {
         if (buff[lend] == '\n' || (lend != 0 && buff[lend - 1] == '\r' && buff[lend] == '\n')) {
             if ((lend != 0 && buff[lend-1] == '\n') ||
@@ -129,12 +158,13 @@ HttpConnection::read_header ()
                 std::string key (buff + lbegin, split - lbegin);
                 std::string val (buff + split + 2, lend - split - 3);
                 header[key] = val;
-                std::cout << "KV (" + key + ", " + val + ")\n";
+                hstr += "  " + key + ": " + val + "\n";
             }
 
             lbegin = lend + 1;
         }
     }
+    LOG_DEBUG (hstr);
 
     std::string KEY_TRANSFER_ENCODING ("Transfer-Encoding");
     if (header[KEY_TRANSFER_ENCODING] == "chunked") {
@@ -147,6 +177,8 @@ HttpConnection::read_header ()
     } else {
         clength = -1;
     }
+
+    return true;
 }
 
 int
@@ -182,8 +214,6 @@ HttpConnection::read (char *retbuff, int len)
         len = socket->read (retbuff, len);
     } else {
         if (buffpos >= bufflen) {
-            delete buff;
-            buff = NULL;
             bufflen = -1;
             buffpos = 0;
 
@@ -197,6 +227,16 @@ HttpConnection::read (char *retbuff, int len)
     }
 
     return len;
+}
+
+int
+HttpConnection::get_response_code ()
+{
+    if (socket) {
+        return response_code;
+    } else {
+        return -1;
+    }
 }
 
 int
--- src/http-connection.h
+++ src/http-connection.h
@@ -24,31 +24,102 @@ class HttpConnection;
 #ifndef __HTTP_CONNECTION_H__
 #define __HTTP_CONNECTION_H__
 
-#include <iostream>
-#include <fstream>
 #include <string>
 #include <map>
 
-#include "download.h"
 #include "url.h"
 #include "socket.h"
 
+/**
+ * Basic pull-style http interface.
+ * Provides a wrapper around the http protocol.  After a request, data can then
+ * be pulled with the read method to get data from the stream.
+ */
 class HttpConnection {
     public:
+        /**
+         * Created un-associated connection.
+         */
         HttpConnection ();
         ~HttpConnection ();
 
+        /**
+         * Initiate a http get request.
+         * Connect to host specified in the url and starts a get request using
+         * the path given in the url.  After the call is completed, the
+         * connection is ready to have data read from it.  Subsequent calls to
+         * any request method will reset the connection and start a new one.
+         * @param url resource to connect to and download
+         * @param start
+         * @param end
+         * @return true if the connection was successful, false otherwise
+         */
         bool send_get_request (Url &url, int start=-1, int end=-1);
-        bool send_head_request (Url &url);
-        bool send_post_request (Url &url, const char *post_data);
 
+        /**
+         * Initiate a http head request.
+         * Connect to host specified in the url and start a head request using
+         * the path given in the url.  After the call is completed, all the
+         * necessary data is retrieved and available with the get_* methods.
+         * Subsiquent calls to any request method will reset the connection and
+         * start a new one.
+         * @param url resource to connect to and retrieve information about
+         * @return true if the connection was successful, false otherwise
+         */
+        bool send_head_request (Url &url);
+
+        /**
+         * Initiate a http post request.
+         * Connect to host specified in the url and start a post request using
+         * the path given in the url and the data given in post_data.  The
+         * post_data is correctly encoded and send in the body of the post
+         * request.  After the call is completed, the connection is ready to
+         * have data read from it.  Subsiquent calls to any request method will
+         * reset the connection and start a new one.
+         * @param url resource to connect to and retrieve information about
+         * @param post_data dictionary to be encoded and send as the body of the
+         *                  post request
+         * @return true if the connection was successful, false otherwise
+         */
+        bool send_post_request (Url &url, std::map<std::string, std::string> &post_data);
+
+        /**
+         * Read data from the connection
+         * Read data from the connection and store it in the buffer passed into
+         * the method.  The method will not necessarily fill the buffer.  The
+         * argument should be used as a maximum value and multiple reads should
+         * be used to get all the data that is required.
+         * @param buff buffer to fill with data
+         * @param len maximum amount of data to read from the connection
+         * @return length of valid data that was written to the buffer, or -1
+         *         indicating that the object is not connected to a valid host
+         */
         int read (char *buff, int len);
 
-        int get_response_code () { return response_code; }
+        /**
+         * Get the http response code for the current request.
+         * @return valid http response code or -1 to indicate that a response
+         *         code is not currently available
+         */
+        int get_response_code ();
+
+        /**
+         * Get the human readable response for the current request.
+         * @return response or empty string if not connected
+         */
         std::string get_code () { return code; }
+
+        /**
+         * Get the current content length.
+         * Fetches the current content length from the download headers.
+         * @return returns content length value or -1 to indicate that the
+         *         server did not specify the length of the content
+         */
         int get_content_length ();
 
     private:
+        static const std::string USER_AGENT;
+
         Url *url;
         Socket *socket;
 
@@ -64,6 +135,7 @@ class HttpConnection {
         int clength;
         int cpos;
 
+        void reset_connection ();
         bool read_header ();
         bool read_line (std::string &line);
 };
